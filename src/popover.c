4a559331f073fb233fb2c1d55d273029d252fe69
tayoky <tayoky4848@gmail.com>
2026-04-11 09:22:27 +0200

    popover : use another surface

--- src/popover.c
+++ src/popover.c
@@ -1,14 +1,16 @@
 #include <popover.h>
 #include <surface.h>
 
-static void tgui_popover_allocate_space(tgui_widget_t *widget) {
-
-
+static void tgui_popover_remove_child(tgui_widget_t *widget, tgui_widget_t *child) {
+	tgui_popover_t *popover = TGUI_POPOVER_CAST(widget);
+	if (popover->child != child) return;
+	// TODO : find a way
 }
 
 static tgui_widget_class_t popover_class = {
 	.size = sizeof(tgui_popover_t),
 	.name = "popover",
+	.remove_child = tgui_popover_remove_child,
 };
 
 tgui_popover_t *tgui_popover_new(void) {
@@ -23,8 +25,10 @@ void tgui_popover_set_child(tgui_popover_t *popover, tgui_widget_t *child) {
 	if (popover->child) {
 		tgui_widget_destroy(popover->child);
 	}
-	tgui_widget_set_parent(child, TGUI_WIDGET_CAST(popover));
-	tgui_widget_set_visible(child, popover->popped);
+	if (popover->surface) {
+		tgui_surface_set_child(popover->surface, child);
+	}
+	popover->child = child;
 }
 
 void tgui_popover_set_position(tgui_popover_t *popover, long x, long y) {
@@ -38,9 +42,18 @@ tgui_widget_t *tgui_popover_get_child(tgui_popover_t *popover) {
 }
 
 void tgui_popover_popdown(tgui_popover_t *popover) {
-	popover->popped = 1;
+	popover->popped = 0;
+	tgui_widget_hide(TGUI_WIDGET_CAST(popover->surface));
 }
 
 void tgui_popover_popup(tgui_popover_t *popover) {
 	popover->popped = 1;
+	if (popover->surface) {
+		tgui_widget_show(TGUI_WIDGET_CAST(popover->surface));
+		return;
+	}
+
+	tgui_widget_calculate_sizes(popover->child);
+	popover->surface = tgui_surface_new(popover->child->pref_width, popover->child->pref_height);
+	tgui_surface_set_child(popover->surface, popover->child);
 }
