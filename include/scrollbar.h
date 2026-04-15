4e3427e9cd78ecd7eb77f87b062468303546816c
tayoky <tayoky4848@gmail.com>
2026-04-15 10:44:28 +0200

    docs : add Doxyfile

--- include/scrollbar.h
+++ include/scrollbar.h
@@ -5,6 +5,10 @@
 #include "slider.h"
 #include "button.h"
 
+/**
+ * @struct tgui_scrollbar
+ * A scrollbar is a special type of slider with buttons at the edges intended for scrolling or zooming.
+ */
 typedef struct tgui_scrollbar {
 	tgui_box_t box;
 	tgui_button_t *top;
@@ -17,6 +21,7 @@ typedef struct tgui_scrollbar {
 #define TGUI_SCROLLBAR_CAST(w) TGUI_CONTAINER_OF(w, tgui_scrollbar_t, box.widget)
 
 /**
+ * @relates tgui_scrollbar
  * @brief create a new scrollbar
  * @param orientation the orientation of the new scrollbar
  * @return a new scrollbar
@@ -24,6 +29,7 @@ typedef struct tgui_scrollbar {
 tgui_scrollbar_t *tgui_scrollbar_new(int orientation);
 
 /**
+ * @relates tgui_scrollbar
  * @brief set the total lenght of the content of a scrollbar
  * @param scrollbar the scrollbar to set the content lenght of
  * @param total_size the total size
@@ -31,6 +37,7 @@ tgui_scrollbar_t *tgui_scrollbar_new(int orientation);
 void tgui_scrollbar_set_total_size(tgui_scrollbar_t *scrollbar, long total_size);
 
 /**
+ * @relates tgui_scrollbar
  * @brief set the lenght of the view of a scrollbar
  * @param scrollbar the scrollbar to set the lenght of
  * @param view_size the size of the view
@@ -38,6 +45,7 @@ void tgui_scrollbar_set_total_size(tgui_scrollbar_t *scrollbar, long total_size)
 void tgui_scrollbar_set_view_size(tgui_scrollbar_t *scrollbar, long view_size);
 
 /**
+ * @relates tgui_scrollbar
  * @brief set the value of a scrollbar
  * @param scrollbar the scrollbar to change the value of
  * @param value the new value
@@ -45,6 +53,7 @@ void tgui_scrollbar_set_view_size(tgui_scrollbar_t *scrollbar, long view_size);
 void tgui_scrollbar_set_value(tgui_scrollbar_t *scrollbar, long value);
 
 /**
+ * @relates tgui_scrollbar
  * @brief add to the value of a scrollbar
  * @param scrollbar the scrollbar to change the value of
  * @param value how much to add to the value
@@ -52,6 +61,7 @@ void tgui_scrollbar_set_value(tgui_scrollbar_t *scrollbar, long value);
 void tgui_scrollbar_add_value(tgui_scrollbar_t *scrollbar, long value);
 
 /**
+ * @relates tgui_scrollbar
  * @brief get the value of a scrollbar
  * @param scrollbar the scrollbar to get the value of
  * @return the value of the scrollbar
