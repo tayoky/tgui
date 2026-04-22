d6954434a96c6257e62da7d5f56bb887cd6e689e
tayoky <tayoky4848@gmail.com>
2026-04-22 19:15:32 +0200

    twm : implement grabing surfaces

--- platform/twm.c
+++ platform/twm.c
@@ -295,9 +295,13 @@ void tgui_platform_set_surface_position(tgui_surface_t *surface, long x, long y)
 }
 
 void tgui_platform_grab_surface(tgui_surface_t *surface) {
+	stanix_window_t *stanix_window = surface->private;
+	twm_grab_input(stanix_window->window, 0);
 }
 
 void tgui_platform_ungrab_surface(tgui_surface_t *surface) {
+	(void)surface;
+	twm_grab_input(TWM_NULL, 0);
 }
 
 void tgui_platform_new_color(tgui_color_t *color, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
