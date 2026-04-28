e69cd19cb227eca96f1c239b70894209ac97c27c
tayoky <tayoky4848@gmail.com>
2026-04-28 19:49:47 +0200

    text buffer : simple text buffer without marks, cursor or deletion

---
+++ src/textbuffer.c
@@ -0,0 +1,151 @@
+#include <textbuffer.h>
+#include <string.h>
+#include <stdlib.h>
+
+TOBJECT_DEFINE_CLASS(tgui_text_buffer, TGUI_TEXT_BUFFER, tobject_get_type())
+
+static int tgui_text_buffer_constructor(void *object) {
+	tgui_text_buffer_get_parent_class()->constructor(object);
+
+	tgui_text_buffer_t *buffer = TGUI_TEXT_BUFFER_CAST(object);
+	buffer->capacity = 1;
+	buffer->lines_count = 1;
+	buffer->lines = malloc(sizeof(tgui_text_buffer_line_t));
+	buffer->lines[0].data = strdup("");
+	buffer->lines[0].size = 0;
+	buffer->lines[0].capacity = 1;
+
+	return 0;
+}
+
+static void tgui_text_buffer_class_init(tgui_text_buffer_class_t *class) {
+	tobject_class_t *tobject_class = TOBJECT_CLASS_CAST(class);
+	tobject_class->constructor = tgui_text_buffer_constructor;
+}
+
+static void append_buf(tgui_text_iter_t *iter, const char *str, size_t size) {
+	size_t new_size = iter->line->size + size;
+	if (iter->line->capacity < new_size) {
+		size_t capacity = iter->line->capacity;
+		while (capacity < new_size) {
+			capacity *= 2;
+		}
+		iter->line->data = realloc(iter->line->data, capacity);
+		iter->line->capacity = capacity;
+	}
+	memmove(iter->line->data + iter->index + size, iter->line->data + iter->index, iter->line->size - iter->index);
+	memcpy(iter->line->data + iter->index, str, size);
+	iter->line->size += size;
+}
+
+static tgui_text_buffer_line_t *append_line(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter) {
+	size_t i = iter->line - buffer->lines + 1;
+	if (buffer->lines_count + 1 > buffer->capacity) {
+		buffer->capacity *= 2;
+		buffer->lines = realloc(buffer->lines, sizeof(tgui_text_buffer_line_t));
+	}
+	memmove(buffer->lines + i + 1, buffer->lines + i, (buffer->lines_count - i) * sizeof(tgui_text_buffer_line_t));
+	buffer->lines[i].data = strdup("");
+	buffer->lines[i].size = 0;
+	buffer->lines[i].capacity = 1;
+	buffer->lines_count++;
+	iter->line = &buffer->lines[i];
+	iter->index = 0;
+
+	return &buffer->lines[i];
+}
+
+static void delete(tgui_text_iter_t *iter, size_t count) {
+	memmove(iter->line->data + iter->index, iter->line->data + iter->index + count, iter->line->size - iter->index - count);
+	iter->line->size -= count;
+}
+
+static void delete_lines(tgui_text_iter_t *iter, size_t count) {
+}
+
+tgui_text_buffer_t *tgui_text_buffer_new(void) {
+	return tobject_new(tgui_text_buffer_get_type());
+}
+
+void tgui_text_buffer_get_start_iter(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter) {
+	iter->line = &buffer->lines[0];
+	iter->index = 0;
+}
+
+void tgui_text_buffer_get_end_iter(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter) {
+	iter->line = &buffer->lines[buffer->lines_count];
+	iter->index = iter->line->size;
+}
+
+void tgui_text_buffer_get_line_iter(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter, size_t line) {
+	if (line >= buffer->lines_count) {
+		tgui_text_buffer_get_end_iter(buffer, iter);
+	}
+	iter->index = 0;
+	iter->line = &buffer->lines[line];
+}
+
+void tgui_text_buffer_get_line_index(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter, size_t line, size_t index) {
+	tgui_text_buffer_get_line_iter(buffer, iter, line);
+	if (iter->index > 0) {
+		// we hit end
+		return;
+	}
+	if (index > iter->line->size) {
+		index = iter->line->size;
+	}
+	iter->index = index;
+}
+
+void tgui_text_buffer_get_line_offset(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter, size_t line, size_t offset) {
+}
+
+void tgui_text_buffer_delete(tgui_text_buffer_t *buffer, tgui_text_iter_t *start, tgui_text_iter_t *end) {
+	// swap if end is before start
+	if (end->line < start->line || (end->line == start->line && end->index < start->index)) {
+		tgui_text_iter_t *tmp = end;
+		end = start;
+		start = tmp;
+	}
+
+	if (start->line == end->line) {
+		delete(start, end->index - start->index);
+		end->index = start->index;
+	} else {
+		// TODO
+	}
+}
+
+void tgui_text_buffer_insert_buf(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter, const char *buf, size_t size) {
+	while (size > 0) {
+		char *newline = memchr(buf, '\n', size);
+		size_t count;
+		if (newline) {
+			count = newline - buf;
+		} else {
+			count = size;
+		}
+		append_buf(iter, buf, count);
+		if (newline) {
+			size -= count - 1;
+			buf += count + 1;
+			append_line(buffer, iter);
+		}
+	}
+	
+}
+
+void tgui_text_buffer_insert(tgui_text_buffer_t *buffer, tgui_text_iter_t *iter, const char *str) {
+	tgui_text_buffer_insert_buf(buffer, iter, str, strlen(str));
+}
+
+void tgui_text_buffer_clear(tgui_text_buffer_t *buffer) {
+	// TODO
+}
+
+void tgui_text_buffer_set_text(tgui_text_buffer_t *buffer, const char *str) {
+	tgui_text_buffer_clear(buffer);
+	tgui_text_iter_t start;
+	tgui_text_buffer_get_start_iter(buffer, &start);
+	tgui_text_buffer_insert(buffer, &start, str);
+}
